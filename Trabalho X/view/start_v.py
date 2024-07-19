79527e37a89813eb991f8984d37b417e12ac53b6
Jhedder <Jheddersenac@gmail.com>
2024-07-19 17:20:51 -0300

    commit

--- Trabalho X/view/start_v.py
+++ Trabalho X/view/start_v.py
@@ -1,45 +1,40 @@
 import tkinter as tk
-from controler.user_c import UsuarioController
-from controler.delete_c import DeleteController
-from controler.atualiza_c import AtualizaController
-from model.usuario_m import UsuarioModel
-from view.user_v import UsuarioView
-from view.delete_v import DeleteView
-from view.atualiza_v import AtualizaView
+from tkinter import ttk
+from tkinter import messagebox
 from PIL import Image, ImageTk
-class StartView(tk.Frame):
+
+class UsuarioView(tk.Frame):
   
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
         self.pack(fill=tk.BOTH, expand=True)
         self.master.geometry('600x600')
-        self.create_background()
         self.create_widgets()
-
-    
-
-    def load_image(self):
-        image = Image.open("C:/Users/182400232/Documents/Trabalho/Trabalho X/Imagens/maxresdefault.jpg")
-        image = image.resize((780, 600), Image.LANCZOS)
-        photo = ImageTk.PhotoImage(image)
-        return photo
-
-    def create_background(self):
-        # Cria um Label para exibir a imagem como plano de fundo
-        self.background_image = self.load_image()
-        self.background_label = tk.Label(self, image=self.background_image)
-        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)  # Posiciona o Label para ocupar toda a área do Frame
-
-
-    def button_click(self):
+        
+def button_click():
         print("Botão clicado!")
+        root.destroy()  # Destroi a janela principal quando o botão é clicado
 
-    def create_widgets(self):
-        # Cria um Frame para centralizar o botão na tela
-        frame_center = tk.Frame(self)
-        frame_center.place(relx=0.5, rely=0.9, anchor=tk.S)
+root = tk.Tk()
+root.title('Megaman X5')
 
-        # Cria o botão dentro do Frame centralizado
-        button = tk.Button(frame_center, text="Iniciar O Jogo", command=self.button_click)
-        button.pack()
+    # Abre a imagem usando PIL (Pillow)
+image = Image.open("C:/Users/182400232/Documents/Trabalho/Trabalho X/Imagens/maxresdefault.jpg")
+
+    # Converte a imagem PIL para um objeto PhotoImage
+photo = ImageTk.PhotoImage(image)
+
+    # Cria um Label para exibir a imagem
+label = tk.Label(root, image=photo)
+label.pack()
+
+    # Cria um Frame para o botão
+frame = tk.Frame(root)
+frame.pack(pady=10)
+
+    # Cria o botão dentro do Frame
+button = tk.Button(frame, text="Iniciar o Jogo", command=button_click)
+button.pack()
+
+root.mainloop()
